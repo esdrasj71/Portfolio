3b8d9534236b082a7131a319b022ae6de27252be
Esdras Jimenez <esdrasj71@gmail.com>
2022-11-02 21:31:20 -0600

    Footer, server and animation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,9 +5,7 @@ import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 import { HashLink } from 'react-router-hash-link'; 
-import {
-  BrowserRouter as Router
-} from "react-router-dom"; 
+import { BrowserRouter as Router } from "react-router-dom"; 
 
 export const NavBar = () => {
 
@@ -46,11 +44,11 @@ export const NavBar = () => {
             <Nav className="ms-auto">
               <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
               <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+              <Nav.Link href="#project" className={activeLink === 'project' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('project')}>Projects</Nav.Link>
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
+                <a href="https://www.linkedin.com/in/esdras-jimenez-a1925b254?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BaYNSEdGXQMaYPgj9pLKHDQ%3D%3D"><img src={navIcon1} alt="" /></a>
                 <a href="#"><img src={navIcon2} alt="" /></a>
                 <a href="#"><img src={navIcon3} alt="" /></a>
               </div>
