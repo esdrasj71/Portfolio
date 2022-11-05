53d698009063b6729c033612daa04d91ac3b7c6e
Esdras Jimenez <esdrasj71@gmail.com>
2022-11-04 22:07:12 -0600

    New project card image

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,7 @@ import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+import projImg4 from "../assets/img/project-img4.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -11,9 +12,9 @@ export const Projects = () => {
 
   const projects = [
     {
-      title: "Modern Bank Webpage",
+      title: "HooBank Webpage",
       description: "Modern UI/UX design using React, Figma, Vite & Tailwind",
-      imgUrl: projImg1,
+      imgUrl: projImg4,
     },
     {
       title: "Business Startup",
